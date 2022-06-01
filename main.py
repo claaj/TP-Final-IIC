59ae2d31faeb8177575511dc8e0a3752c72e9a68
Matías Cajal <matias.cajal@hotmail.com>
2022-06-01 00:26:34 -0300

    Primera implementación menú principal

--- main.py
+++ main.py
@@ -5,6 +5,22 @@
 # TP Final
 ################
 
+def menuPrincipal ():
+    """
+    Función del menú principal del cajero.
+    """
+    opcion = 0
+    while opcion != 4:
+        opcion = int(input('1)Consultas 2)Retiros 3)Transferencias 4)Salir:'))
+        if opcion == 1:
+            print('Consultas')
+        elif opcion == 2:
+            print('Retiros')
+        elif opcion == 3:
+            print('Transferencia')
+
 #Main
 if __name__ == '__main__':
     print('Hola mundo!')
+    menuPrincipal()
+    print('Programa finalizado!')
